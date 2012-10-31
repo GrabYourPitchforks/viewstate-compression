d9de16cfa8fb2f4a7989a7c7920886078444f1d9
GrabYourPitchforks <levib@yahoo.com>
2012-10-31 16:35:35 -0700

    v1.0 of compression routine

---
+++ CompressedHiddenFieldPageStatePersister.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Web.UI;
+
+namespace ViewStateCompressor
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class CompressedHiddenFieldPageStatePersister : HiddenFieldPageStatePersister
+    {
+        public CompressedHiddenFieldPageStatePersister(Page page) : base(page) { }
+
+        public override void Load()
+        {
+            // depend on HiddenFieldPageStatePersister for heavy lifting and crypto
+            base.Load();
+
+            CompressedSerializedData compressedData = ViewState as CompressedSerializedData;
+            if (compressedData == null && ControlState != null)
+            {
+                // the underlying data was not compressed
+                return;
+            }
+
+            // decompress
+            using (MemoryStream uncompressedStream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(uncompressedStream, CompressionMode.Decompress, leaveOpen: true))
+                {
+                    zipStream.Write(compressedData.RawData, 0, compressedData.RawData.Length);
+                }
+
+                uncompressedStream.Position = 0;
+                ObjectStateFormatter formatter = new ObjectStateFormatter();
+                Pair pair = (Pair)formatter.Deserialize(uncompressedStream);
+
+                // extract
+                ViewState = pair.First;
+                ControlState = pair.Second;
+            }
+        }
+
+        public override void Save()
+        {
+            using (MemoryStream uncompressedStream = new MemoryStream())
+            {
+                ObjectStateFormatter formatter = new ObjectStateFormatter();
+                formatter.Serialize(uncompressedStream, new Pair(ViewState, ControlState));
+
+                using (MemoryStream compressedStream = new MemoryStream())
+                {
+                    using (GZipStream zipStream = new GZipStream(compressedStream, CompressionLevel.Optimal, leaveOpen: true))
+                    {
+                        zipStream.Write(uncompressedStream.GetBuffer(), 0, checked((int)uncompressedStream.Length));
+                    }
+
+                    if (uncompressedStream.Length > compressedStream.Length)
+                    {
+                        // compressing will probably save space
+                        // CompressedSerializeData uses BinaryFormatter, which ObjectStateFormatter serializes better than byte[]
+                        ViewState = new CompressedSerializedData() { RawData = compressedStream.ToArray() };
+                        ControlState = null;
+                    }
+
+                    // depend on HiddenFieldPageStatePersister for heavy lifting and crypto
+                    base.Save();
+                }
+            }
+        }
+
+        [Serializable]
+        private sealed class CompressedSerializedData
+        {
+            public byte[] RawData;
+        }
+    }
+}
---
+++ ViewStateCompressorControlBuilderInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.Compilation;
+using System.Web.UI;
+
+namespace ViewStateCompressor
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class ViewStateCompressorControlBuilderInterceptor : ControlBuilderInterceptor
+    {
+        public override void OnProcessGeneratedCode(ControlBuilder controlBuilder, CodeCompileUnit codeCompileUnit, CodeTypeDeclaration baseType, CodeTypeDeclaration derivedType, CodeMemberMethod buildMethod, CodeMemberMethod dataBindingMethod, System.Collections.IDictionary additionalState)
+        {
+            // only run this once during page compilation, and only use this one builder (so that we don't get master pages, etc.)
+            if (controlBuilder.GetType() == typeof(FileLevelPageControlBuilder))
+            {
+                // the page will only contain one namespace and one type
+                var ns = codeCompileUnit.Namespaces.Cast<CodeNamespace>().FirstOrDefault();
+                if (ns != null)
+                {
+                    var type = ns.Types.Cast<CodeTypeDeclaration>().FirstOrDefault();
+                    if (type != null)
+                    {
+                        /* When this is output, it will inject this into every page:
+                         * 
+                         * protected override PageStatePersister PageStatePersister {
+                         *   get { return new CompressedHiddenFieldPageStatePersister(this); }
+                         * }
+                         * 
+                         */
+                        CodeMemberProperty property = new CodeMemberProperty()
+                        {
+                            Name = "PageStatePersister",
+                            HasGet = true,
+                            Attributes = MemberAttributes.Override | MemberAttributes.Family,
+                            Type = new CodeTypeReference(typeof(PageStatePersister))
+                        };
+                        var newObj = new CodeObjectCreateExpression(typeof(CompressedHiddenFieldPageStatePersister), new CodeThisReferenceExpression());
+                        property.GetStatements.Add(new CodeMethodReturnStatement(newObj));
+                        type.Members.Add(property);
+                    }
+                }
+            }
+
+            base.OnProcessGeneratedCode(controlBuilder, codeCompileUnit, baseType, derivedType, buildMethod, dataBindingMethod, additionalState);
+        }
+    }
+}
